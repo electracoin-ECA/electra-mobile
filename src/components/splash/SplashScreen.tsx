5e6bf5b811bfdb64053540976fb488d351b3a631
Durrab Khan <durrab@Durrabs-MacBook-Pro.local>
2018-06-10 22:54:48 -0700

    indentation changes to splash screen

--- src/components/splash/SplashScreen.tsx
+++ src/components/splash/SplashScreen.tsx
@@ -13,8 +13,8 @@ interface UserSession {
   sessionId: string
 }
 interface SplashScreenProps {
- name: string,
- navigation: any
+  name: string,
+  navigation: any
 }
 
 interface SplashScreenState {
@@ -36,116 +36,97 @@ export default class SplashScreen extends React.Component<SplashScreenProps, Spl
     };
   }
 
-  componentDidMount() {
-        //get the Unique ID of the Device
-        //Create the Request and send it
-        this.setState({status:'Verifying...'});
-        AsyncStorage.removeItem('electra-device-id');
-        this.checkDeviceIdInStorage();
-        this.setState({status:'Loading...'});
+componentDidMount() {
+  this.setState({status:'Verifying...'});
+  AsyncStorage.removeItem('electra-device-id');
+  this.checkDeviceIdInStorage();
+  this.setState({status:'Loading...'});
+}
 
-
+private async checkDeviceIdInStorage() {
+  let deviceId = await this.getDeviceId();
+  if(deviceId === null){
+    this.verifyDeviceId(false);
+  }else {
+    this.verifyDeviceId(true);
   }
+}
 
-  private async checkDeviceIdInStorage() {
+private async verifyDeviceId(isEncrypted:boolean){
+  let deviceId = DeviceInfo.getUniqueID();
+  let electraUserRequest = {
+    deviceId: deviceId,
+    channel: 'web',
+    language: 'us',
+    encrypted: isEncrypted
+  };
 
-    let deviceId = await this.getDeviceId();
+  try{
+    let electraRequest = JSON.stringify(electraUserRequest);
+    let response = await fetch(Config.API_URL+'/createSession', {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: electraRequest,
+    });
 
-    if(deviceId === null){
-
-        //send the request to back end and retrieve the encrypted device id
-        //store it in local storage and navigate the login screen
-        this.verifyDeviceId(false);
-    }else {
-      //send the encrypted device id to the back end and verify the encrypted device id
-      //and check the status if the user should navigate to login or registration screen
-      this.verifyDeviceId(true);
-    }
-
+    let electraResponse = await response.json();
+    this.saveDeviceId(electraResponse.deviceId);
+    this.navigateUserOnResponse(electraResponse.status);
   }
-
-  private async verifyDeviceId(isEncrypted:boolean){
-    let deviceId = DeviceInfo.getUniqueID();
-    let electraUserRequest = {
-      deviceId: deviceId,
-      channel: 'web',
-      language: 'us',
-      encrypted: isEncrypted
-    };
-    try{
-
-      let electraRequest = JSON.stringify(electraUserRequest);
-      let response = await fetch(Config.API_URL+'/createSession', {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: electraRequest,
-      });
-
-      let electraResponse = await response.json();
-      this.saveDeviceId(electraResponse.deviceId);
-      this.navigateUserOnResponse(electraResponse.status);
-    }
-    catch(error){
-      Alert.alert('Error Occured While Fetching: ' + error);
-    }
-
-
-
+  catch(error){
+    Alert.alert('Error Occured While Fetching: ' + error);
   }
+}
 
-  render() {
-     const {height: screenHeight} = Dimensions.get('window');
-    if(this.state.isLoading) {
-      return this.renderCurrentAction(screenHeight);
-    }
-    return;
+render() {
+  const {height: screenHeight} = Dimensions.get('window');
+  if(this.state.isLoading) {
+    return this.renderCurrentAction(screenHeight);
   }
+  return;
+}
 
-  private renderCurrentAction(screenHeight: any){
-      return (
-        <Container style={{flexDirection: 'row', justifyContent: 'center', backgroundColor: '#2e1325'}}>
-          <Content>
-            <View style={{flex: 1, height: screenHeight, alignItems: 'center', justifyContent: 'center'}}>
-              <ElectraSpinner animate={true}/>
-              <Text style={{ fontSize: 20, color:'#FFFFFF' }}>{this.state.status}</Text>
-           </View>
-          </Content>
-        </Container>
-      )
+private renderCurrentAction(screenHeight: any){
+  return (
+    <Container style={{flexDirection: 'row', justifyContent: 'center', backgroundColor: '#2e1325'}}>
+      <Content>
+        <View style={{flex: 1, height: screenHeight, alignItems: 'center', justifyContent: 'center'}}>
+          <ElectraSpinner animate={true}/>
+          <Text style={{ fontSize: 20, color:'#FFFFFF' }}>{this.state.status}</Text>
+        </View>
+      </Content>
+    </Container>
+  )
+}
+
+private saveDeviceId(deviceId: string){
+  AsyncStorage.setItem('electra-device-id', deviceId);
+}
+
+private async getDeviceId() {
+  let deviceId = null;
+  try {
+    deviceId = await AsyncStorage.getItem('electra-device-id');
   }
-
-  private saveDeviceId(deviceId: string){
-      AsyncStorage.setItem('electra-device-id', deviceId);
+  catch(error) {
+    Alert.alert('Cannot retrieve Device Id: '+error);
   }
+  return deviceId;
+}
 
-  private async getDeviceId() {
-    let deviceId = null;
-    try {
-        deviceId = await AsyncStorage.getItem('electra-device-id');
-    }
-    catch(error) {
-       Alert.alert('Cannot retrieve Device Id: '+error);
-    }
-
-    return deviceId;
+private navigateUserOnResponse(status: string) {
+  if(status === 'ACTIVE'){
+    this.props.navigation.navigate('Login');
   }
-
-  private navigateUserOnResponse(status: string) {
-
-    if(status === 'ACTIVE'){
-      //Modal of Login Screen
-      this.props.navigation.navigate('Login');
-    }
-    else if(status === 'NEW' || 'REGISTER') {
-      //Modal of Registration Screen
-      this.props.navigation.navigate('Registration');
-    }
-    else if(status === 'BLOCKED') {
-      //Alert user that the user is blockedd
-    }
+  else if(status === 'NEW' || 'REGISTER') {
+    this.props.navigation.navigate('Registration');
   }
+  else if(status === 'BLOCKED') {
+    Alert.alert('Device is Blocked. Please contact support');
+  }
+}
 
 }
